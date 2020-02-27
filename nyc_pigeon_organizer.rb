02d708daa4fd10046b5ea89e189e7cf922ec56b9
Alessandro Duterte <alexduterte@gmail.com>
2020-02-27 04:30:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,18 +1,18 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
   result = {}
-  data.each do |key, value|
-    value.each do |key2, name|
-      name.each do |name|
-        if !result[name]
+  data.each do |key, value|  #// grabs each key "color gender lives"
+    value.each do |key2, name| #// first run colors, then gender, then live
+      name.each do |name| #// checks name per attatribute
+        if !result[name]  #// if this name doesnt exist it creates it into the hash
           result[name] = {}
         end
 
-        if !result[name][key]
+        if !result[name][key] #// if this key doesnt exist for the name, create it
           result[name][key] = []
         end
 
-        result[name][key] << key2.to_s
+        result[name][key] << key2.to_s #// pushes value into the key
       end
     end
   end
